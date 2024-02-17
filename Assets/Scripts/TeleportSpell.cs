5c1b684427b0cb11a8f9f8af38d85fe6ef20d394
Kaajolee <latoza959@gmail.com>
2024-02-17 16:14:59 +0200

    teleport prototype

---
+++ Assets/Scripts/TeleportSpell.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class TeleportSpell : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float maxDistance;
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            Teleport();
+        }
+    }
+    Vector3 FindDestination()
+    {
+        RaycastHit hit;
+        Vector3 destination;
+        if(Physics.Raycast(transform.position, transform.forward, out hit, maxDistance))
+        {
+            Debug.Log("pataike");
+            destination = hit.point;
+            return destination;
+        }
+        else
+        {
+            Debug.Log("Nepataike");
+            return transform.position;
+        }
+            
+        
+    }
+    void Teleport()
+    {
+        Vector3 destination = FindDestination();
+        transform.position = destination;
+        Physics.SyncTransforms();
+    }
+}
