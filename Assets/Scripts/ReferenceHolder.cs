1890f50ea68669a94b5d73fa6f3ab1ffa4a757c0
Kaajolee <latoza959@gmail.com>
2024-07-23 20:31:33 +0300

    Update ReferenceHolder.cs

--- Assets/Scripts/ReferenceHolder.cs
+++ Assets/Scripts/ReferenceHolder.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ReferenceHolder : MonoBehaviour
 {
     public GameObject ContentGO;
+    [AllowNull]
     public GameObject CurrentItemDropGO;
     public Scrollbar scrollbar;
     public Button TakeAllButton;
